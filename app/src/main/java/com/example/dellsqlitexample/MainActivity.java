86060a578baecdfc0df75ac300cd8611576b70b5
madasilva1 <madasilva195@gmail.com>
2021-12-16 14:01:36 -0500

    Search button replaced by update button

--- app/src/main/java/com/example/dellsqlitexample/MainActivity.java
+++ app/src/main/java/com/example/dellsqlitexample/MainActivity.java
@@ -13,61 +13,53 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText name, loc, desig,delet,sear;
-    Button saveBtn,deletebtn,searchBtn;
+    EditText name, loc, desig, delet, sear;
+    Button saveBtn, deletebtn, updatehBtn;
     Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        DbHandler helper ;
+        DbHandler helper;
         helper = new DbHandler(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        delet = (EditText)findViewById(R.id.delete);
-        name = (EditText)findViewById(R.id.txtName);
-        loc = (EditText)findViewById(R.id.txtLocation);
-        desig = (EditText)findViewById(R.id.txtDesignation);
-        sear = (EditText)findViewById(R.id.search);
-        searchBtn = (Button)findViewById(R.id.searchBtn);
-        deletebtn = (Button)findViewById(R.id.deleteBtn);
+        delet = (EditText) findViewById(R.id.delete);
+        name = (EditText) findViewById(R.id.txtName);
+        loc = (EditText) findViewById(R.id.txtLocation);
+        desig = (EditText) findViewById(R.id.txtDesignation);
+        sear = (EditText) findViewById(R.id.search);
+        updatehBtn = (Button) findViewById(R.id.updatehBtn);
+        deletebtn = (Button) findViewById(R.id.deleteBtn);
         deletebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String DeleteUser = delet.getText().toString();
+                if (DeleteUser.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Delete unsuccessfully", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-        saveBtn = (Button)findViewById(R.id.btnSave);
-        saveBtn.setOnClickListener(new View.OnClickListener()
-
-        {
+        saveBtn = (Button) findViewById(R.id.btnSave);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
             public void onClick(View v) {
-                String username = name.getText().toString()+"\n";
+                String username = name.getText().toString() + "\n";
                 String location = loc.getText().toString();
                 String designation = desig.getText().toString();
                 DbHandler dbHandler = new DbHandler(MainActivity.this);
-                dbHandler.insertUserDetails(username,location,designation);
-                intent = new Intent(MainActivity.this,DetailsActivity.class);
+                dbHandler.insertUserDetails(username, location, designation);
+                intent = new Intent(MainActivity.this, DetailsActivity.class);
                 startActivity(intent);
-                Toast.makeText(getApplicationContext(), "Details Inserted Successfully",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Details Inserted Successfully", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    public void deleteBtn (View view) {
 
-     String DeleteUser = delet.getText().toString();
-     if(DeleteUser.isEmpty()){
-         Toast.makeText(getApplicationContext(), "Delete unsuccessfully",Toast.LENGTH_SHORT).show();
-     }
-     else{
-
-
-
-     }
-    }
 }
+
 
